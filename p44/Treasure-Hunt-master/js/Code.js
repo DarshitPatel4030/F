3618ef4584f27763b118735f1a170340c4332907
DarshitPatel4030 <80969037+DarshitPatel4030@users.noreply.github.com>
2021-07-11 21:33:02 +0530

    Add files via upload

---
+++ p44/Treasure-Hunt-master/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("E I T M ", 100,50)
+    fill("lightblue")
+    text("Hint : NEVER WAITS FOR ANYONE NOT EVEN FOR VIP !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("R T E H A ", 700,150)
+    fill("lightblue")
+    text("Hint: one & only planet that contain oxygen !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("W I E T H A H T", 100,320)
+    fill("lightblue")
+    text("Hint: the best coding class !!", 100,340)
+
+}
